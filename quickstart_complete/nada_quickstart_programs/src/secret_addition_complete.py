18875fd4be97d2f107d34b4eaf47df75f1d91bb0
NavaneethKrishnaS <navaneethself@gmail.com>
2024-07-05 12:48:34 +0000

    my new nada program

--- quickstart_complete/nada_quickstart_programs/src/secret_addition_complete.py
+++ quickstart_complete/nada_quickstart_programs/src/secret_addition_complete.py
@@ -1,12 +1,17 @@
 from nada_dsl import *
+
 def nada_main():
-
     party1 = Party(name="Party1")
-
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
-
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
-
     new_int = my_int1 + my_int2
-
     return [Output(new_int, "my_output", party1)]
+
+nada_program = NADAProgram(nada_main)
+telemetry_identifier = "your_telemetry_identifier"
+nada_program.enable_telemetry(telemetry_identifier)
+nada_program.compile()
+nada_program.execute()
+
+print("NADAProgram created and telemetry enabled successfully.")
+
